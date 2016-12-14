5e0c58512416161ed56e6ff3a3736b0f090f8514
Seth <sethduffin@gmail.com>
2016-12-13 21:46:11 -0700

    Added admin view

--- BlueFrame/Website/javascript.js
+++ BlueFrame/Website/javascript.js
@@ -1,4 +1,5 @@
 $( document ).ready(function() {
+    $("#passcodePage").hide()
 	$("body").css("display", "none");
     $("body").fadeIn(200);
 	$("a").click(function(event){
@@ -10,4 +11,49 @@ $( document ).ready(function() {
     function redirectPage() {
         window.location = linkLocation;
     }
+    $('.num').click(function () {
+        var num = $(this);
+        var text = $.trim(num.find('.txt').clone().children().remove().end().text());
+        var passbox = $('#passbox');
+        $(passbox).val(passbox.val() + text);
+    });
 });
+
+function unlock() {
+    if($('#passbox').val() == password) {
+        $('#passcodePage').animate({
+          opacity: "0"
+        }, 500 );
+        setTimeout(function(){
+            $('#passcodePage').hide()
+        
+        }, 500);
+        setTimeout(function(){
+            $('#passbox').val('')
+            window.open("https://github.com/ItsMurf/BlueFrame","_self")
+        }, 500);
+    }
+    else {
+        console.log("yes")
+        $('#passbox').animate({
+          backgroundColor: "#ff6060",
+          color: "#fff",
+        }, 500 );
+        setTimeout(function(){
+            $('#passbox').animate({
+              backgroundColor: "white"
+            }, 500 );
+
+        }, 500);
+        setTimeout(function(){
+            $('#passbox').val('')
+            $('#passbox').css('color', 'black')
+        }, 1000);
+    }
+}
+function enter(event) {
+    if (event.which == 13) {
+        unlock()
+    }
+}
+password = "*0"
